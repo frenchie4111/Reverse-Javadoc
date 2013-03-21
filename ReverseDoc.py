e6b661da39a7883dad84e3aee08ed6c74b0033ea
Michael Lyons <mdl7240@rit.edu>
2013-03-21 14:29:41 -0400

    Added BeautifulSoup library. Added method class

--- ReverseDoc.py
+++ ReverseDoc.py
@@ -1,1 +1,29 @@
-i
+import sys
+from bs4 import BeautifulSoup
+
+class method:
+    __slots__ = ("comments", "return_type", "method_name")
+
+    def get_method_text(self):
+        #Returns the text of the method
+        return self.comments + "\n\tpublic" + self.return_type + self.method_name + "() {" + "\n" + "//Body" + "\n" + "}"
+
+def find_methods_summary( html_summary ):
+    print html_summary.prettify()
+    for table_row in html_summary.find_all("tr"):
+        print "ROW"
+
+def find_methods( html ):
+    methods_list = list()
+    soup = BeautifulSoup( html )
+    method_summary = soup.find("a", {"name":"method_summary"} ,recursive="true").findNext("table")
+    find_methods_summary( method_summary )
+    #first_method_return = method_summary.findNext("tr").findNext("tr").find("td").find("code").text.strip()
+
+def main():
+    print( "Hello, World" )
+    input_html = sys.stdin.read()
+    find_methods( input_html )
+
+if(__name__ == '__main__'):
+    main()
---
+++ TestReverseDoc.py
@@ -0,0 +1,23 @@
+import unittest
+
+# import the module(s) to be tested:
+# from template import *
+from ReverseDoc import *
+
+class TestTemplate(unittest.TestCase):
+
+    # setUp - run prior to the start of each test case
+    def setUp(self):
+	# initialize test fixtures
+        return
+
+    def test_smoke(self):
+        # very simple test to insure test framework is correct
+        # replace this assert with meaningful test 
+        self.assertTrue(1)
+
+    # additional test_xxx methods follow....
+    
+	
+if __name__ == '__main__':
+    unittest.main()
